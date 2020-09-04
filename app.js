d3fd6345ded50236867bc6543391440089ad3e08
ADoseOfOpinion <69084299+ADoseOfOpinion@users.noreply.github.com>
2020-09-03 20:00:36 -0400

    a functional view more button has been created

--- app.js
+++ app.js
@@ -14,3 +14,31 @@ searchBar.addEventListener('keyup', function(e){
     }
 })
 */
+
+/*
+.text = .bloglist
+.moreText = .readmorechunk1
+.viewmoreblogs = .read-more-btn
+*/
+
+
+
+const ViewMoreButton = document.querySelector('.viewmoreblogs');
+const BlogList = document.querySelector('.bloglist');
+const BlogChunk = document.querySelector('.readmorechunk1');
+
+ViewMoreButton.addEventListener('click', (e) => {
+    BlogList.classList.toggle('show-more');
+    ViewMoreButton.classList.toggle('show-less')
+    if (ViewMoreButton.innerText === "View More Blogs") {
+        ViewMoreButton.style.display = block;
+        BlogChunk.style.display = none;
+    } else {
+        ViewMoreButton.style.display = none;
+        BlogChunk.style.display = block;
+    }
+});
+
+
+
+
