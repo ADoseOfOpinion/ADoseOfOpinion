93c68cb6dd382a77269a2e614df25ec63892764f
ADoseOfOpinion <69084299+ADoseOfOpinion@users.noreply.github.com>
2020-12-27 12:54:51 -0500

    button fix attempt

--- app.js
+++ app.js
@@ -23,19 +23,44 @@ searchBar.addEventListener('keyup', function(e){
 
 
 
+//the button that is clicked on to show earliest blogs
 const ViewMoreButton = document.querySelector('.viewmoreblogs');
+//one big div for all blogs
 const BlogList = document.querySelector('.bloglist');
+//second earliest blogs
+const BlogChunk2 = document.querySelector('.readmorechunk2');
+//earliest blogs
 const BlogChunk = document.querySelector('.readmorechunk1');
+
+
+/*ViewMoreButton.addEventListener('click', (e) => {
+    BlogList.classList.toggle('show-more');
+    ViewMoreButton.classList.toggle('show-less')
+    if (ViewMoreButton.innerText === "View More Blogs") {
+        ViewMoreButton.style.display = block;
+        BlogChunk2.style.display = none;
+    } else {
+        ViewMoreButton.style.display = none;
+        BlogChunk2.style.display = block;
+    }
+}); */
+
 
 ViewMoreButton.addEventListener('click', (e) => {
     BlogList.classList.toggle('show-more');
     ViewMoreButton.classList.toggle('show-less')
     if (ViewMoreButton.innerText === "View More Blogs") {
         ViewMoreButton.style.display = block;
+        BlogChunk2.style.display = none;
+        BlogChunk.style.display = none;
+    } else if (ViewMoreButton.clicked = true) {
+        ViewMoreButton.style.display = block;
+        BlogChunk2.style.display = block;
         BlogChunk.style.display = none;
     } else {
         ViewMoreButton.style.display = none;
-        BlogChunk.style.display = block;
+        BlogChunk2.style.display = none;
+        BlogChunk.style.display = none;
     }
 });
 
